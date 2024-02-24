cab559f5d12964888212de8d6aa377d0defa0fdb
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-02-24 22:55:39 +0000

    add logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import '../css/Header.css';
+import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header>
+    <header className="header-div">
+      <img src={logo} alt="logo" />
       <h1 className="header">GUESTBOOK</h1>
     </header>
   );
